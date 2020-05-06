dd1c02d4c222e3e3a3f3450571ec9376cc0e5ee0
Nadya Nadhita <63330161+nadyanadhita@users.noreply.github.com>
2020-05-06 16:35:27 +0700

    Add files via upload

---
+++ database/migrations/2020_05_02_040603_create_pengeluarans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengeluaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengeluarans', function (Blueprint $table) {
+            $table->id();
+            $table->string('hari');
+            $table->date('tanggal');
+            $table->integer('nominal');
+            $table->string('kategori2');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengeluarans');
+    }
+}
